75759608ce5be25c65b35fa57cd1dae2ab57db34
shlee <seokhyun90@gmail.com>
2021-04-28 23:17:22 +0900

    20210428 fetch axios

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Movie app 2019
 
 React JS fundamentals Course (Nomadcoders, 2019 updated)
+
+### Requirement
+
+- axios
--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import propTypes from "prop-types";
 import React from 'react';
+import axios from 'axios';
 
 
 class App extends React.Component{ 
-  
-  // state는 object 이다
-  // state의 값을 변경할 땐 항상 setState를 사용한다.
-  // setState를 사용할 때 마다 새로운 state와 함께 render()를 호출한다.
-  state = {  
-    count: 0
+    state = {  
+    isLoading: true
   }
 
-  add = () => {
-    // this.setState({count: this.state.count + 1})
-    this.setState(current => ({count: current.count + 1}))
-  }
+getMovies = async () => {
+  const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+};
 
-  minus = () =>{
-    this.setState(current => ({count: current.count - 1}))
-  }
-  
 componentDidMount(){
-  console.log("Component rendered");
+  this.getMovies();
 }
 
 componentDidUpdate(){
@@ -30,13 +22,9 @@ componentDidUpdate(){
 
   render(){    // React는  Class component의 render 메소드를 자동으로 실행
     console.log("I'm rendering");
+    const { isLoading } = this.state;   // ES6 Magic
     return (
-    <div>
-      <h1>The number is : {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
-    
+      <div>{isLoading ? "Loading..." : "We are Ready"}</div>
     )
   }
 }
